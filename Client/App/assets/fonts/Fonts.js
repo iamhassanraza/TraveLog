c171ebc8c3d93ade72427067d887c439014f7e99
Hassan Raza <36694573+iamhassanraza@users.noreply.github.com>
2019-10-08 18:32:04 +0500

    redux added , debugger added , front end structure completed

---
+++ Client/App/assets/fonts/Fonts.js
@@ -0,0 +1,9 @@
+//Create A java script object with style.create function and include all the fonts in the app
+
+import {styleSheet} from 'react-native';
+
+export default fonts = styleSheet.create({
+  font1: {
+    //write css
+  },
+});
---
+++ Client/App/assets/icons/Icons.js
@@ -0,0 +1,1 @@
+//import icons from libary ionicans and the export it from here to use throughout your app
