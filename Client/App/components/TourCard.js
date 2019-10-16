78d29e71649655585836f93ad59e97444ba0f001
iamhassanraza <m.h.raxa1@gmail.com>
2019-10-16 14:55:21 +0500

    vector icons integrated

--- Client/App/components/TourCard.js
+++ Client/App/components/TourCard.js
@@ -2,27 +2,37 @@ import React from 'react'
 import { View, Text , StyleSheet,ImageBackground} from 'react-native'
 import  { ThemeColor } from '../assets/Colors/Colors'
 import image from '../assets/images/2.jpg'
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 const TourCard = () => {
     return (
         <View style={styles.Container}>
             <View style={styles.ImageContainer}>
                 <ImageBackground source={image} style={styles.ImageContainer}>
-                <View style={{backgroundColor:'blue',height:50,flexDirection:'column',justifyContent:'flex-end'}}>
-                    <Text style={{...styles.TourCardHeading,color:'white'}}>SKARDU</Text>
-                    </View>
-                  
-                   
-                  
+                    <View style={{padding:2,backgroundColor: "rgba(0,0,0,0.5)",height:32,flexDirection:'column',justifyContent:'flex-end'}}>
+                            <View>
+                                <Text style={{...styles.TourCardHeading,color:'white',fontSize:22}}>Trip to SKARDU</Text>
+                            </View>
+                    </View>   
                 </ImageBackground>
             </View>
-    
-        
-        <View>
 
-        <Text style={styles.TourCardHeading}>Noman Adventure Society </Text>
-        <Text style={styles.TourCardDate}><Text>From: </Text>9 Oct<Text> to </Text>20 Oct, 2019</Text>
+          <View style={styles.TextConatiner}>
+                <View style={{borderWidth:0.3,height:50,width:50}}>
+
+                </View>
+        
+                <View style={{paddingLeft:2}}>
+                        <Text style={styles.TourCardDate}><Text>From: </Text>9 Oct<Text> to </Text>20 Oct, 2019</Text>
+                        <Text style={styles.TourCardHeading}>Noman Adventure Society </Text>
+                        <Icon name="rocket" size={30} color="#900" />
+                        
+                </View>
+
+                <View style={{borderWidth:0.3,height:50,width:50}}>
+
+                </View>
         </View>
+                
         </View>
     )
 }
@@ -43,25 +53,25 @@ const styles = StyleSheet.create({
     ImageContainer:{
     
         height:190,  
-
+        flexDirection:'column',
+        justifyContent:'flex-end'
     },
     TourCardHeading:{
-        fontSize:18,
+        fontSize:14,
         fontWeight:'bold',
-        color: ThemeColor
+        color: ThemeColor,
+        
        
     },
     TourCardDate:{
-        fontSize:12
+        fontSize:12,
+        color:'red'
     },
-    giveOpacity:{
-        width:300,
-        height:200,
-        backgroundColor:'black',
-        borderColor:'black',
-        borderWidth:5,
-        padding:30,
-        margin:20,
+    TextConatiner:{
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'space-around'
         
     }
+   
 })
