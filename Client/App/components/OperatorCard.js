d24f98fa247c68e32c32702ef7aff45c4c9c299a
faraznaqvi99 <faraznaqvi99@gmail.com>
2019-10-21 14:48:30 +0500

    height wala bhanda

--- Client/App/components/OperatorCard.js
+++ Client/App/components/OperatorCard.js
@@ -20,7 +20,7 @@ class OperatorCard extends React.Component{
     render() {
         return (
             <>  
-            <View style={{borderColor: ThemeColor,borderWidth: 2, marginTop: 60, height: Dimensions.get("window").height/2.4, width: '80%'}}>
+            <View style={{borderColor: ThemeColor,borderWidth: 2, marginTop: 60, width: '80%'}}>
                 <ImageBackground source={image} style={{height: 100}}>
 
                 </ImageBackground>
