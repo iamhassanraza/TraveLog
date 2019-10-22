c3d0d2c6e74f1dea5569131dcbde7eaaa02f53a5
Ali Mehdi <salimehdi144@gmail.com>
2019-10-22 17:44:19 +0500

    DB deleted

--- Client/App.js
+++ Client/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 import Home from './App/screens/Home'
+import DestinationDetails from './App/screens/DestinationDetails';
 
 export default class App extends Component {
   render() {
     return (
       <View>
-    <Home></Home>
+    <DestinationDetails></DestinationDetails>
       </View>
     )
   }
