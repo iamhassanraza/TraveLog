711cfbe1df21b17d8c0efcee47f8d7b230892fbb
Ali Mehdi <salimehdi144@gmail.com>
2019-10-29 14:56:07 +0500

    Baad me krunga modalll ka

--- Client/App/screens/Modal.js
+++ Client/App/screens/Modal.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react'
-import { Text, View, ScrollView,FlatList,Dimensions, ImageBackground} from 'react-native'
+// import React, { Component } from 'react'
+// import { Text, View, ScrollView,FlatList,Dimensions, ImageBackground} from 'react-native'
 
-import image from "../assets/images/1.jpg"
-import FlatListContainer from '../components/FlatListContainer'
-import ForModal from '../components/ForModal'
-import image2 from "../assets/images/2.jpg"
+// import image from "../assets/images/1.jpg"
+// import FlatListContainer from '../components/FlatListContainer'
+// import ForModal from '../components/ForModal'
+// import image2 from "../assets/images/2.jpg"
 
 
-export default class Flayt extends Component {
+// export default class Flayt extends Component {
 
   
 
-    render() {
+//     render() {
 
-        const DATA = [
-            { i: image},
-            { i: image2},
-            { i: image},
-            { i: image2},
-            { i: image2},
-            { i: image},
-            { i: image2},
-            { i: image},
-            { i: image2},
-            { i: image},
-            { i: image},
-            { i: image2}
+//         const DATA = [
+//             { i: image},
+//             { i: image2},
+//             { i: image},
+//             { i: image2},
+//             { i: image2},
+//             { i: image},
+//             { i: image2},
+//             { i: image},
+//             { i: image2},
+//             { i: image},
+//             { i: image},
+//             { i: image2}
            
-        ];
+//         ];
     
-        return (
-            <FlatListContainer style={{marginLeft:3}} title="Album">
-                <FlatList
-                horizontal
-                data={DATA}
-                showsHorizontalScrollIndicator={false}
-                renderItem={({ item }) => <ForModal i ={item.i}>
-                </ForModal>}
-                />
+//         return (
+//             <FlatListContainer style={{marginLeft:3}} title="Album">
+//                 <FlatList
+//                 horizontal
+//                 data={DATA}
+//                 showsHorizontalScrollIndicator={false}
+//                 renderItem={({ item }) => <ForModal i ={item.i}>
+//                 </ForModal>}
+//                 />
                 
                 
                 
-            </FlatListContainer>
-        )
-    }
-}
+//             </FlatListContainer>
+//         )
+//     }
+// }
