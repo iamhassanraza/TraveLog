9e939d9a2f24af3079bbc3640d4e3aa036d8a337
Ali Mehdi <salimehdi144@gmail.com>
2019-10-31 19:01:51 +0500

    Modal ban rha hai

--- Client/App.js
+++ Client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import DestinationDetails from './App/screens/TourDetail';
+import DestinationDetails from './App/screens/Modal';
 
 export default class App extends Component {
   render() {
