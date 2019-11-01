7974d7365c2ae6eaa5394fe966b4a95c44bc15e4
Ali Mehdi <salimehdi144@gmail.com>
2019-11-01 18:06:15 +0500

    End for the Weekend

--- Client/App/screens/Modal.js
+++ Client/App/screens/Modal.js
@@ -6,47 +6,66 @@ import {
   Alert,
   TouchableWithoutFeedback,
   Button,
+  StyleSheet
 } from 'react-native';
 import {ThemeColor} from '../assets/Colors/Colors';
 import Modal from 'react-native-modal';
-import {CheckBox} from 'react-native-elements';
-
-import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
 export default class ModalTester extends Component {
   state = {
-    isModalVisible: true
-   
+    isModalVisible: true,
   };
 
   toggleModal = () => {
     this.setState({isModalVisible: !this.state.isModalVisible});
   };
 
-  
-
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, }}>
         <Button title="Show modal" onPress={this.toggleModal} />
         <Modal
           isVisible={this.state.isModalVisible}
           onBackdropPress={() => this.setState({isModalVisible: false})}>
-          <View style={{flex: 1}}>
+          <View style={{flex: 1, justifyContent: 'flex-end'}}>
             <Text style={{color: 'white'}}>Hello!</Text>
+            <Text
+              onPress={() => alert('For Boys')}
+              style={styles.TextWithNavigation}>
+              For Boys
+            </Text>
 
-           
-              
-            <Text onPress={()=> alert('For Family')} style={{color:"white", fontSize:17,paddingBottom:'3%'}} > For Family</Text>
+            <Text
+              onPress={() => alert('For Family')}
+              style={styles.TextWithNavigation}>
+              For Family
+            </Text>
 
+            <Text
+              onPress={() => alert('For Girls')}
+              style={styles.TextWithNavigation}>
+              For Girls
+            </Text>
 
+            <Text onPress={() => alert('For Honeymoon')} style={styles.TextWithNavigation}>
+              Honeymoon Trips
+            </Text>
 
-
-
-            <Button title="Hide modal" onPress={this.toggleModal} />
+            {/* <Button title="Hide modal" onPress={this.toggleModal} /> */}
           </View>
         </Modal>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  TextWithNavigation: {
+    color: 'white',
+    fontSize: 17,
+    margin: 1,
+    paddingLeft: '4%',
+    backgroundColor: ThemeColor,
+    padding:"3%"
+  },
+});
