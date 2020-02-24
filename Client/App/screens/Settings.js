a8e23a34b6ef55fae3e91077f89000161dcd962e
a4m4 <salimehdi144@gmail.com>
2020-02-24 14:27:33 +0500

    settigs

--- Client/App/screens/Settings.js
+++ Client/App/screens/Settings.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ScrollView, StyleSheet } from 'react-native'
+import IconWithText from '../components/IconWithText'
+
 
 export default class Settings extends Component {
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
+            <ScrollView>
+
+<View style={{marginLeft:"8%", marginTop:"5%"}}>
+
+<IconWithText icon="phone" title="Contact Us" iconstyle={styles.IconStyle} textstyle={styles.TextStyle}></IconWithText>
+<IconWithText icon="phone" title="Terms & Conditions" iconstyle={styles.IconStyle} textstyle={styles.TextStyle}></IconWithText>
+<IconWithText icon="phone" title="Help" iconstyle={styles.IconStyle} textstyle={styles.TextStyle}></IconWithText>
+<IconWithText icon="phone" title="Support" iconstyle={styles.IconStyle} textstyle={styles.TextStyle}></IconWithText>
+<IconWithText icon="phone" title="Change Password" iconstyle={styles.IconStyle} textstyle={styles.TextStyle}></IconWithText>
+<IconWithText icon="phone" title="Logout" iconstyle={styles.IconStyle} textstyle={styles.TextStyle}></IconWithText>
+
+</View>
+
+
+            </ScrollView>
         )
     }
 }
+
+
+
+const styles = StyleSheet.create({
+    IconStyle: {
+        fontSize:26,paddingTop:5
+    },
+    TextStyle:{
+fontSize:29
+    }
+})
