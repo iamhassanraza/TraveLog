69e137ea9306b6f1bd8159b5e21535980f065516
iamhassanraza <m.h.raxa1@gmail.com>
2020-03-17 16:55:40 +0500

    webview

--- Client/App/screens/webview.js
+++ Client/App/screens/webview.js
@@ -4,6 +4,8 @@ import {WebView} from 'react-native-webview';
 
 export default class webview extends Component {
   render() {
-    return <WebView source={{uri: 'https://reactnative.dev/'}} />;
+    return <WebView 
+    userAgent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+    source={{uri: 'https://travelog-pk.herokuapp.com/auth/google'}} />;
   }
 }
